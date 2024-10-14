44f72890334064ae525a40bc770f1269a9cc4e7f
Rudrank Riyam <rudrankriyam@gmail.com>
2024-10-14 12:00:15 +0530

    Make the library static

--- Package.swift
+++ Package.swift
@@ -17,13 +17,16 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MeshingKit",
+            type: .static,
             targets: ["MeshingKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MeshingKit"),
-
+            name: "MeshingKit",
+            resources: [
+                .process("Resources/gradient.jpg")
+            ]),
     ]
 )
