d1a8c4de876e3112648eeecabbcb4ae4292cab05
Rudrank Riyam <rudrankriyam@gmail.com>
2024-10-14 12:01:22 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,29 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "MeshingKit",
-    platforms: [
-        .iOS(.v18),
-        .macOS(.v15),
-        .macCatalyst(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11),
-        .visionOS(.v2)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MeshingKit",
-            type: .static,
-            targets: ["MeshingKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MeshingKit",
-            resources: [
-                .process("Resources/gradient.jpg")
-            ]),
-    ]
+  name: "MeshingKit",
+  platforms: [
+    .iOS(.v18),
+    .macOS(.v15),
+    .macCatalyst(.v18),
+    .tvOS(.v18),
+    .watchOS(.v11),
+    .visionOS(.v2)
+  ],
+  products: [
+    .library(
+      name: "MeshingKit",
+      type: .static,
+      targets: ["MeshingKit"]),
+  ],
+  targets: [
+    .target(
+      name: "MeshingKit"
+    )
+  ]
 )
